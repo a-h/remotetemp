2dbb3db3a371ed21711c0b8031ce9b4236cc17bb
Adrian Hesketh <adrianhesketh@hushmail.com>
2020-09-28 20:49:49 +0100

    Added push of data.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
@@ -10,12 +11,12 @@
 
 #include "DHT.h"
 
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTPIN 2 // Digital pin connected to the DHT sensor
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
 // Uncomment whatever type you're using!
-#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT11 // DHT 11
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
@@ -32,7 +33,8 @@
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Serial.println(F("DHTxx test!"));
 
@@ -55,7 +57,10 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-void loop() {
+char data[100];
+
+void loop()
+{
   // Wait a few seconds between measurements.
   delay(2000);
 
@@ -64,17 +69,14 @@ void loop() {
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h) || isnan(t))
+  {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
@@ -83,10 +85,23 @@ void loop() {
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
   Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
+  Serial.print(F("°C  Heat index: "));
   Serial.print(hic);
   Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
+
+  HTTPClient https;
+  WiFiClientSecure secureClient;
+  secureClient.setInsecure();
+  Serial.print("[HTTP] begin...\n");
+  //TODO: Change this to be your API address.
+  if (https.begin(secureClient, "xxxxx.execute-api.us-east-1.amazonaws.com", 443, "/", true))
+  {
+    sprintf(data, "{\"temp\": %f}", t);
+    int code = https.POST(data);
+    String string = https.getString();
+    Serial.println(code);
+    Serial.println(string);
+  }
+  https.end();
+  secureClient.stop();
 }
